0a1caa8fcafd1999ec282eac8de101f1aa6dc917
Rasmus Larsson <rasmus.larsson@gmail.com>
2014-01-05 23:53:50 +0100

    Added a simple api for commonly used strings.

---
+++ agent/src/restapi.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function() {
+
+	//---------------
+	// Internals
+	//---------------
+	var version = 'v1';
+
+	//---------------
+	// Module exports
+	//---------------
+    module.exports = {
+		FILES: '/rest/' + version + '/files/',
+		NOTIFICATIONS : '/rest/' + version + '/notifications/',
+		TRANSFERS: '/rest/' + version + '/transfers/',
+		AGENTS: '/rest/' + version + '/agents/'
+	};
+}());
